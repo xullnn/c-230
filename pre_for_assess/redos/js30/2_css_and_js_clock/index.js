83ebbcc7f1205186f0fcf888b8087a87657be3b3
gitCavendish <xll290016537@gmail.com>
2020-06-02 11:35:57 +0800

    2 of js30 finished

---
+++ pre_for_assess/redos/js30/2_css_and_js_clock/index.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+  let secondHand = document.querySelector('.second-hand'),
+        minuteHand = document.querySelector('.min-hand'),
+        hourHand = document.querySelector('.hour-hand');
+
+  function setDate() {
+    let now = new Date(),
+          seconds = now.getSeconds(),
+          mins = now.getMinutes(),
+          hours = now.getHours();
+
+    let secondsDegrees = ((seconds / 60) * 360) + 90,
+          minsDegrees = ((mins + seconds / 60) / 60 * 360) + 90,
+          hoursDegrees = ((hours + mins / 60) / 12) * 360 + 90;
+
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${minsDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+  };
+
+  setInterval(setDate, 100);
+})
